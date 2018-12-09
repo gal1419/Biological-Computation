53f2cd548f0b0ba7a4345d0e187565750c32d4ff
Gal Malachi <malachi@hpe.com>
2018-12-09 08:15:54 +0200

    finalize genetics

--- src/genetic-algorithms/Environment.js
+++ src/genetic-algorithms/Environment.js
@@ -1,114 +1,119 @@
 import React from "react";
 import configuration from "./conf";
-import helper from './helper-service';
-import './Environment.css';
+import helper from "./helper-service";
+import "./Environment.css";
 
 class Environment extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            environment: helper.generateEnvironment(),
-            speed: 100
-        };
-    }
-
-    start = () => {
-        const initialPopulation = helper.generatePopulation(this.state.environment);
-        console.log(initialPopulation);
-        // this.timerID = setInterval(
-        //     () =>
-        //         this.setState({
-        //         }),
-        //     this.state.speed
-        // );
+    this.state = {
+      environment: helper.generateEnvironment(),
+      population: null,
+      speed: 100
     };
+  }
 
-    stop = () => {
-        clearInterval(this.timerID);
-        clearInterval(this.graphTimerId);
-        this.showGraph = false;
+  start = () => {
+    this.setState({
+      population: helper.generateInitialPopulation(this.state.environment)
+    });
+
+    this.timerID = setInterval(
+      () =>
         this.setState({
-            isRunning: false,
-            showGraph: false
-        });
-    };
+          population: helper.calculateNextGeneration(
+            this.state.population,
+            this.state.environment
+          )
+        }),
+      this.state.speed
+    );
+  };
 
-    restart = () => {
-        this.stop();
-        this.restartValues();
-        this.setState({
-        });
-    };
+  stop = () => {
+    clearInterval(this.timerID);
+    clearInterval(this.graphTimerId);
+    this.showGraph = false;
+    this.setState({
+      isRunning: false,
+      showGraph: false
+    });
+  };
 
-    componentWillUnmount() {
-        this.stop();
-    }
+  restart = () => {
+    this.stop();
+    this.restartValues();
+    this.setState({});
+  };
 
-    restartValues = () => {
-    };
+  componentWillUnmount() {
+    this.stop();
+  }
 
-    selectCell = event => {
-        console.log(event.target);
-        this.setState({
-            //selectedCell: cellData
-        });
-    };
+  restartValues = () => {};
 
-    createEnvironment = () => {
-        const table = [];
-        const size = configuration.gridSize;
+  selectCell = event => {
+    console.log(event.target);
+    this.setState({
+      //selectedCell: cellData
+    });
+  };
 
-        for (let i = 0; i < size; i++) {
-            let children = [];
+  createEnvironment = () => {
+    const table = [];
+    const size = configuration.gridSize;
 
-            for (let j = 0; j < size; j++) {
-                let cellIndex = size * i + j;
-                children.push(
-                    <div
-                        key={cellIndex}
-                        onClick={this.selectCell}
-                        className={this.state.environment[i][j].type}
-                    />
-                );
-            }
-            table.push(children);
-        }
-        return table;
-    };
+    for (let i = 0; i < size; i++) {
+      let children = [];
 
-    render = () => {
-        return (
-            <div className="grid-container">
-                <div className="info-section">
-                    <button
-                        onClick={!this.state.isRunning ? this.start : this.stop}
-                        className="start-button"
-                    >
-                        {!this.state.isRunning ? "Start Simulation" : "Stop Simulation"}
-                    </button>
-                    <button onClick={this.restart} className="start-button">
-                        Restart Simulation
-          </button>
-                    <button onClick={this.generateGraphB} className="start-button">
-                        Generate Graph B
-          </button>
-                    <button onClick={this.generateGraphC} className="start-button">
-                        Generate Graph C
-          </button>
-                </div>
-                <div className="grid-section">
-                    <div className="environment">{this.createEnvironment()}</div>
-                    <div className="buttons">
-                        <button onClick={this.toggleEarth}>Toggle Earth</button>
-                        <button onClick={this.toggleHeatMap}>Toggle Heat Map</button>
-                        <button onClick={this.toggleClouds}>Toggle Clouds</button>
-                        <button onClick={this.toggleRain}>Toggle Rain</button>
-                    </div>
-                </div>
-            </div>
+      for (let j = 0;j  < size; j++) {
+        let cellIndex = size * i + j;
+        children.push(
+          <div
+            key={cellIndex}
+            onClick={this.selectCell}
+            className={this.state.environment[i][j].type}
+          />
         );
-    };
+      }
+      table.push(children);
+    }
+    return table;
+  };
+
+  render = () => {
+    return (
+      <div className="grid-container">
+        <div className="info-section">
+          <button
+            onClick={!this.state.isRunning ? this.start : this.stop}
+            className="start-button"
+          >
+            {!this.state.isRunning ? "Start Simulation" : "Stop Simulation"}
+          </button>
+          <button onClick={this.restart} className="start-button">
+            Restart Simulation
+          </button>
+          <button onClick={this.generateGraphB} className="start-button">
+            Generate Graph B
+          </button>
+          <button onClick={this.generateGraphC} className="start-button">
+            Generate Graph C
+          </button>
+        </div>
+        <div className="grid-section">
+          <div className="environment">{this.createEnvironment()}</div>
+          <div className="buttons">
+            <button onClick={this.toggleEarth}>Toggle Earth</button>
+            <button onClick={this.toggleHeatMap}>Toggle Heat Map</button>
+            <button onClick={this.toggleClouds}>Toggle Clouds</button>
+            <button onClick={this.toggleRain}>Toggle Rain</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
 }
 
 export default Environment;
--- src/genetic-algorithms/helper-service.js
+++ src/genetic-algorithms/helper-service.js
@@ -2,13 +2,13 @@ import configuration from "./conf";
 import _ from "lodash";
 
 class HelperService {
-  CHROMOSOMELENGTH = 40;
-  FITNESS_COEFFICIENT = 1000000;
   TARGET_COORDINATES = {
     x: configuration.gridSize - 1,
     y: configuration.gridSize - 1
   };
+
   obstacles = [];
+
   neighborDirs = [
     [-1, -1],
     [0, -1],
@@ -38,103 +38,6 @@ class HelperService {
     return grid;
   };
 
-  generatePopulation = grid => {
-    const population = [];
-    const populationSize = configuration.populationSize;
-
-    for (let i = 0; i < populationSize; i++) {
-      const chromosome = {
-        coordinates: [{ x: 0, y: 0 }],
-        distanceFromTarget: undefined,
-        fitness: undefined
-      };
-
-      for (let j = 0; j < this.CHROMOSOMELENGTH - 1; j++) {
-        const { x, y } = chromosome.coordinates[j];
-        const neighbors = this.getNeighbors(x, y, grid);
-        chromosome.coordinates[j + 1] = this.getRandomNeighborCoordinates(
-          neighbors
-        );
-      }
-      chromosome.distanceFromTarget = this.getDistanceFromTarget(
-        chromosome.coordinates[this.CHROMOSOMELENGTH - 1]
-      );
-      population.push(chromosome);
-    }
-
-    return this.calculatePopulationFitness(population);
-  };
-
-  calculatePopulationFitness(population) {
-    population.forEach(chromosome => {
-      this.calculateChromosomeFitness(chromosome);
-    });
-
-    return _.sortBy(population, ["fitness"]);
-  }
-
-  calculateChromosomeFitness(chromosome) {
-    let score = 0;
-    const { coordinates } = chromosome;
-    const hasObstacles = !_.isEmpty(
-      _.intersectionWith(this.obstacles, coordinates, _.isEqual)
-    );
-    const indexOfTargetCoordinateInPath = _.findIndex(coordinates, coordinate =>
-      _.isEqual(coordinate, this.TARGET_COORDINATES)
-    );
-
-    const lastCoordinateInPath =
-      indexOfTargetCoordinateInPath >= 0
-        ? coordinates[indexOfTargetCoordinateInPath]
-        : coordinates[coordinates.length - 1];
-
-    const pathLength =
-      indexOfTargetCoordinateInPath >= 0
-        ? indexOfTargetCoordinateInPath
-        : this.CHROMOSOMELENGTH;
-
-    const distanceFromTarget = this.getDistanceFromTarget(lastCoordinateInPath);
-
-    score = pathLength / this.CHROMOSOMELENGTH;
-    score = hasObstacles ? score + 1 : score;
-    score =
-      distanceFromTarget === 0
-        ? distanceFromTarget
-        : distanceFromTarget / Math.pow(configuration.gridSize, 2);
-
-    chromosome.fitness = score;
-  }
-
-  getDistanceFromTarget(coordinates) {
-    return Math.sqrt(
-      Math.pow(coordinates.x - this.TARGET_COORDINATES.x, 2) +
-        Math.pow(coordinates.y - this.TARGET_COORDINATES.y, 2)
-    );
-  }
-
-  getRandomNeighborCoordinates = neighbors => {
-    const keys = Object.keys(neighbors);
-    const neighbor = neighbors[keys[Math.floor(Math.random() * keys.length)]];
-    return this.getCoordinatesByIndex(neighbor.index);
-  };
-
-  getCoordinatesByIndex = index => {
-    return {
-      x: Math.floor(index / configuration.gridSize),
-      y: index % configuration.gridSize
-    };
-  };
-
-  getNeighbors = (x, y, grid) => {
-    const neighbors = this.neighborDirs.map(([dX, dY]) => [x + dX, y + dY]);
-    const gridNeighbors = neighbors.map(([i, j]) =>
-      this.getFromGrid(grid, i, j)
-    );
-    return gridNeighbors.filter(v => v);
-  };
-
-  getFromGrid = (grid, x, y) => (grid[x] || [])[y];
-
   getCellType = (cellIndex, i, j) => {
     if (cellIndex === 0) {
       return "robot";
@@ -147,7 +50,7 @@ class HelperService {
     return "empty";
   };
 
-  isObstacle = i => {
+  isObstacle(i) {
     if (!configuration.hasObstacles) {
       return false;
     }
@@ -160,7 +63,385 @@ class HelperService {
     }
 
     return false;
+  }
+
+  generateInitialPopulation = grid => {
+    const population = [];
+    const populationSize = configuration.populationSize;
+
+    for (let i = 0; i < populationSize; i++) {
+      const chromosome = this.createNewChoromosome([{ x: 0, y: 0 }]);
+      this.generateChromoseInitialPath(chromosome, grid);
+      population.push(chromosome);
+    }
+
+    return this.calculatePopulationFitnessDistanceAndObstacles(population);
   };
+
+  createNewChoromosome(coordinates) {
+    return {
+      coordinates: coordinates || [],
+      distanceFromTarget: undefined,
+      fitness: undefined,
+      hasObstacles: undefined
+    };
+  }
+
+  generateChromoseInitialPath(chromosome, grid) {
+    const { coordinates } = chromosome;
+    const chromosomeLength = this.getRandomInt(
+      10,
+      Math.pow(configuration.gridSize, 2)
+    );
+    for (let j = 0; j < chromosomeLength; j++) {
+      const neighbors = this.getNeighbors(coordinates[j], grid);
+      const neighborsCoordinates = _.map(neighbors, n =>
+        this.getCoordinatesByIndex(n.index)
+      );
+      _.remove(neighborsCoordinates, n => _.some(coordinates, n));
+
+      if (_.isEmpty(neighborsCoordinates)) return;
+
+      coordinates[j + 1] = this.getRandomNeighborCoordinates(
+        neighborsCoordinates
+      );
+    }
+  }
+
+  calculatePopulationFitnessDistanceAndObstacles(population) {
+    population.forEach(chromosome => {
+      this.calculateDistanceFromTarget(chromosome);
+      this.calculateObstacles(chromosome);
+      this.calculateChromosomeFitness(chromosome);
+    });
+
+    return _.sortBy(population, ["fitness"]);
+  }
+
+  calculateDistanceFromTarget(chromosome) {
+    const { coordinates } = chromosome;
+    const indexOfTargetCoordinateInPath = this.getIndexOfTargetCoordinateInPath(
+      coordinates
+    );
+    const lastCoordinateInPath =
+      indexOfTargetCoordinateInPath >= 0
+        ? coordinates[indexOfTargetCoordinateInPath]
+        : coordinates[coordinates.length - 1];
+
+    chromosome.distanceFromTarget = this.getPointDistanceFromTargetPoint(
+      lastCoordinateInPath.x,
+      lastCoordinateInPath.y
+    );
+  }
+
+  getPointDistanceFromTargetPoint(x, y) {
+    return Math.sqrt(
+      Math.pow(x - this.TARGET_COORDINATES.x, 2) +
+        Math.pow(y - this.TARGET_COORDINATES.y, 2)
+    );
+  }
+
+  calculateObstacles(chromosome) {
+    chromosome.hasObstacles = _.isEmpty(
+      _.intersectionWith(this.obstacles, chromosome.coordinates, _.isEqual)
+    );
+  }
+
+  calculateChromosomeFitness(chromosome) {
+    let fitness = 0;
+    const { coordinates, distanceFromTarget, hasObstacles } = chromosome;
+    const indexOfTargetCoordinateInPath = this.getIndexOfTargetCoordinateInPath(
+      coordinates
+    );
+
+    const pathLength =
+      indexOfTargetCoordinateInPath >= 0
+        ? indexOfTargetCoordinateInPath
+        : coordinates.length;
+
+    fitness = pathLength / Math.pow(configuration.gridSize, 2);
+    fitness += hasObstacles ? 1 : 0;
+    fitness +=
+      distanceFromTarget === 0
+        ? distanceFromTarget
+        : distanceFromTarget / configuration.gridSize;
+
+    chromosome.fitness = fitness;
+  }
+
+  getIndexOfTargetCoordinateInPath(coordinates) {
+    return _.findIndex(coordinates, coordinate =>
+      _.isEqual(coordinate, this.TARGET_COORDINATES)
+    );
+  }
+
+  getObstablesInPath(coordinates) {
+    return _.intersectionWith(this.obstacles, coordinates, _.isEqual);
+  }
+
+  calculateNextGeneration = (oldPopulation, grid) => {
+    const newPopulation = [];
+    const totalFitness = _.sumBy(
+      oldPopulation,
+      chromosome => chromosome.fitness
+    );
+    const oldPopulationClone = _.cloneDeep(oldPopulation);
+    const elitisemIndex = Math.ceil(oldPopulation.length * 0.05);
+    const elitisemSelection = oldPopulationClone.slice(0, elitisemIndex);
+    Array.prototype.push.apply(newPopulation, elitisemSelection);
+
+    while (newPopulation.length < configuration.populationSize) {
+      const firstParent = this.rouletteWheelSelection(
+        oldPopulationClone,
+        totalFitness
+      );
+      const secondParent = this.rouletteWheelSelection(
+        oldPopulationClone,
+        totalFitness
+      );
+
+      if (_.isEqual(firstParent.coordinates, secondParent.coordinates)) {
+        continue;
+      }
+
+      const offspring = this.crossover(firstParent, secondParent);
+
+      if (offspring && offspring.coordinates.length > 10) {
+        this.mutate(offspring, grid);
+        newPopulation.push(offspring);
+      }
+    }
+    return this.calculatePopulationFitnessDistanceAndObstacles(newPopulation);
+  };
+
+  crossover = (firstParent, secondParent) => {
+    const sharedCoordinates = _.intersectionWith(
+      firstParent.coordinates,
+      secondParent.coordinates,
+      _.isEqual
+    );
+
+    _.remove(sharedCoordinates, { x: 0, y: 0 });
+
+    if (_.isEmpty(sharedCoordinates)) {
+      return null;
+    }
+
+    const firstParentIndexOfTargetCoordinate = this.getIndexOfTargetCoordinateInPath(
+      firstParent.coordinates
+    );
+    const secondParentIndexOfTargetCoordinate = this.getIndexOfTargetCoordinateInPath(
+      secondParent.coordinates
+    );
+
+    if (
+      firstParentIndexOfTargetCoordinate < 0 &&
+      secondParentIndexOfTargetCoordinate < 0
+    ) {
+      return this.handleCrossoverWithoutTargetPoint(
+        firstParent,
+        secondParent,
+        sharedCoordinates
+      );
+    }
+
+    return this.handleCrossoverWithTargetPoint(
+      firstParent,
+      secondParent,
+      firstParentIndexOfTargetCoordinate,
+      secondParentIndexOfTargetCoordinate,
+      sharedCoordinates
+    );
+  };
+
+  handleCrossoverWithoutTargetPoint(
+    firstParent,
+    secondParent,
+    sharedCoordinates
+  ) {
+    const potenialOffSprings = this.getPotentialOffsprings(
+      sharedCoordinates,
+      firstParent,
+      secondParent
+    );
+
+    return potenialOffSprings[
+      this.getRandomInt(0, potenialOffSprings.length - 1)
+    ];
+  }
+
+  createOffspringByCrossover(
+    parentACoordinates,
+    parentBCoordinates,
+    parentAIndex,
+    ParentBIndex
+  ) {
+    const offspringCoordinates = [
+      ...parentACoordinates.slice(0, parentAIndex),
+      ...parentBCoordinates.slice(ParentBIndex, parentBCoordinates.length - 1)
+    ];
+
+    return this.createNewChoromosome(offspringCoordinates);
+  }
+
+  handleCrossoverWithTargetPoint(
+    firstParent,
+    secondParent,
+    firstParentIndexOfTargetCoordinate,
+    secondParentIndexOfTargetCoordinate,
+    sharedCoordinates
+  ) {
+    let firstParentToUse = secondParent;
+    let targetParentToUse = firstParent;
+
+    let targetPointIndexToUse = firstParentIndexOfTargetCoordinate;
+    _.remove(sharedCoordinates, this.TARGET_COORDINATES);
+
+    if (
+      firstParentIndexOfTargetCoordinate > 0 &&
+      secondParentIndexOfTargetCoordinate > 0
+    ) {
+      targetPointIndexToUse = _.min(
+        firstParentIndexOfTargetCoordinate,
+        secondParentIndexOfTargetCoordinate
+      );
+    } else if (secondParentIndexOfTargetCoordinate > 0) {
+      targetPointIndexToUse = secondParentIndexOfTargetCoordinate;
+    }
+
+    if (!_.isEqual(firstParent[targetPointIndexToUse])) {
+      firstParentToUse = firstParent;
+      targetParentToUse = secondParent;
+    }
+
+    const randomSharedPoint =
+      sharedCoordinates[this.getRandomInt(0, sharedCoordinates.length - 1)];
+    let firstIndex = _.findIndex(firstParent.coordinates, coordinate =>
+      _.isEqual(coordinate, randomSharedPoint)
+    );
+    let secondIndex = _.findIndex(secondParent.coordinates, coordinate =>
+      _.isEqual(coordinate, randomSharedPoint)
+    );
+
+    return this.createOffspringByCrossover(
+      firstParentToUse.coordinates,
+      targetParentToUse.coordinates,
+      firstIndex,
+      secondIndex + 1
+    );
+  }
+
+  getPotentialOffsprings(sharedCoordinates, firstParent, secondParent) {
+    const potenialOffSprings = [];
+    sharedCoordinates.forEach(c => {
+      let firstIndex = _.findIndex(firstParent.coordinates, coordinate =>
+        _.isEqual(coordinate, c)
+      );
+      let secondIndex = _.findIndex(secondParent.coordinates, coordinate =>
+        _.isEqual(coordinate, c)
+      );
+
+      potenialOffSprings.push(
+        this.createOffspringByCrossover(
+          firstParent.coordinates,
+          secondParent.coordinates,
+          firstIndex,
+          secondIndex
+        )
+      );
+
+      potenialOffSprings.push(
+        this.createOffspringByCrossover(
+          secondParent.coordinates,
+          firstParent.coordinates,
+          secondIndex,
+          firstIndex
+        )
+      );
+    });
+
+    return potenialOffSprings;
+  }
+
+  mutate = (offspring, grid) => {
+    const { coordinates } = offspring;
+    const mutationProbability = this.getRandomInt(1, 10);
+
+    if (mutationProbability <= 2) {
+      const obstacles = this.getObstablesInPath(coordinates);
+      let indexToMutate = this.getRandomInt(1, coordinates.length - 2);
+
+      if (!_.isEmpty(obstacles)) {
+        indexToMutate = _.findIndex(coordinates, coordinate =>
+          _.isEqual(coordinate, obstacles[0])
+        );
+      }
+
+      if (!coordinates[indexToMutate - 1] || !coordinates[indexToMutate + 1]) {
+        return;
+      }
+
+      coordinates[indexToMutate] = this.getRandomNeighborCoordinates(
+        this.getSharedNeighbors(
+          coordinates[indexToMutate - 1],
+          coordinates[indexToMutate + 1],
+          grid
+        )
+      );
+    }
+  };
+
+  getSharedNeighbors(firstCoordinate, secondCoordinate, grid) {
+    const firstNeighbors = _.map(this.getNeighbors(firstCoordinate, grid), n =>
+      this.getCoordinatesByIndex(n.index)
+    );
+    const secondNeighbors = _.map(
+      this.getNeighbors(secondCoordinate, grid),
+      n => this.getCoordinatesByIndex(n.index)
+    );
+
+    return _.intersectionWith(firstNeighbors, secondNeighbors, _.isEqual);
+  }
+
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  rouletteWheelSelection(population, totalFitness) {
+    let seed = Math.floor(Math.random() * totalFitness);
+
+    for (let i = 0; i < population.length; i++) {
+      const chromosome = population[i];
+      const { fitness } = chromosome;
+      seed -= fitness;
+      if (seed < 0) return chromosome;
+    }
+
+    return population.length - 1;
+  }
+
+  getRandomNeighborCoordinates = neighbors => {
+    const keys = Object.keys(neighbors);
+    return neighbors[keys[Math.floor(Math.random() * keys.length)]];
+  };
+
+  getCoordinatesByIndex = index => {
+    return {
+      x: Math.floor(index / configuration.gridSize),
+      y: index % configuration.gridSize
+    };
+  };
+
+  getNeighbors = (coordinate, grid) => {
+    const { x, y } = coordinate;
+    const neighbors = this.neighborDirs.map(([dX, dY]) => [x + dX, y + dY]);
+    const gridNeighbors = neighbors.map(([i, j]) =>
+      this.getFromGrid(grid, i, j)
+    );
+    return gridNeighbors.filter(v => v);
+  };
+
+  getFromGrid = (grid, x, y) => (grid[x] || [])[y];
 }
 
 export default new HelperService();
